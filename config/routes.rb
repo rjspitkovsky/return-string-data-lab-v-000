2d709c4de9e64f9b620bba3745cc4a0dbc592783
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-05-17 18:20:57 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   get 'home', to: 'static#home'
   resources :orders
   resources :invoices
+
+  resources :products, only: [:index]
+  get '/products/:id/description', to: 'products#description'
 end
